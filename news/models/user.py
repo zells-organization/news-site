028e7f42e23b1c3a0125eaab9221c4fdf983dbb0
vovapasko <vovapasko1699@gmail.com>
2020-05-02 14:08:37 +0300

    Provided custom user

---
+++ news/models/user.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from ..managers.user_manager import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    birthdate = models.DateField(_('birth date'), null=True, blank=True)
+    date_created=models.DateTimeField(_('date created'), auto_now_add=True)
+    date_updated=models.DateTimeField(_('date updated'), auto_now=True)
+    is_superuser=models.BooleanField(_('superuser'), default=False)
+    is_staff=models.BooleanField(_('staff'), default=False)
+
+    objects=UserManager()
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+
+    class Meta:
+        verbose_name=_('user')
+        verbose_name_plural=_('users')
+
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name='%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
