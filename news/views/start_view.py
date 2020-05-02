1b4475013629232b157a8601bd5e865002b1959e
vovapasko <vovapasko1699@gmail.com>
2020-05-02 13:10:51 +0300

    Provided templates and registered app news

--- news/views/start_view.py
+++ news/views/start_view.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render
 
 
 class StartView(View):
+    template_name = 'hello.html'
+
     def get(self, request):
-        # <view logic>
-        return HttpResponse('result')
+        return render(request, self.template_name)
