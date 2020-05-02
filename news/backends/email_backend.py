4a0e8017aeac932495370e064f32d6c62f97302a
vovapasko <vovapasko1699@gmail.com>
2020-05-02 16:48:44 +0300

    Fixed email backend

--- news/backends/email_backend.py
+++ news/backends/email_backend.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from news.models import User
-
+from django.core.exceptions import ValidationError
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
@@ -8,8 +8,8 @@ class EmailBackend(ModelBackend):
         try:
             user = User.objects.get(email=username)
         except User.DoesNotExist:
-            raise ValueError(f"{username} does not exist")
+            raise ValidationError(f"{username} does not exist")
         else:
             if user.check_password(password):
                 return user
-        return ValueError("Incorrect password")
+        raise ValidationError("Incorrect password")
