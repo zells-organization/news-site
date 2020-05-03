125dfe31353cb3cb276a0ac90832c5ee4749d121
vovapasko <vovapasko1699@gmail.com>
2020-05-03 17:29:23 +0300

    Provided panels for premoderation and profile

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('', MainView.as_view(), name='main'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('premoderation/', PremoderationView.as_view(), name='premoderation'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
