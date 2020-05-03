f8bc82f9cd4b1ea096201466fa199d93ab63a2ae
vovapasko <vovapasko1699@gmail.com>
2020-05-03 16:26:00 +0300

    Remastered models. Made BaseModel abstract

--- news/models/base_model.py
+++ news/models/base_model.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class BaseModel(models.Model):
     date_created = models.DateTimeField(_('date created'), auto_now_add=True)
     date_updated = models.DateTimeField(_('date updated'), auto_now=True)
+
+    class Meta:
+        abstract = True
