98fc511215b92667b1d18b5593da77ba334d1ba5
vovapasko <vovapasko1699@gmail.com>
2020-05-09 21:51:30 +0300

    Added str methods to comment and post

--- news/models/post.py
+++ news/models/post.py
@@ -7,3 +7,6 @@ class Post(BaseModel):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     text = models.TextField()
     is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.author} - {self.text}"
