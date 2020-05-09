66db8922c9255ca421d148da0a981227269cc8d2
vovapasko <vovapasko1699@gmail.com>
2020-05-09 21:59:54 +0300

    Provided field title for post

--- news/models/post.py
+++ news/models/post.py
@@ -5,8 +5,9 @@ from .user import User
 
 class Post(BaseModel):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     is_approved = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.author} - {self.text}"
+        return f"{self.author} - {self.title}"
