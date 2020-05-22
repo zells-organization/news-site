98189cd52b3ba58784fe621d6600d398c58bf250
vovapasko <vovapasko1699@gmail.com>
2020-05-22 17:10:45 +0300

    Adding permission for post moderation. Provided command to populate data

---
+++ news/management/commands/populate.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.core.management import BaseCommand
+from news.lib import ADMIN_GROUP_NAME, EDITOR_GROUP_NAME, USER_GROUP_NAME
+from news.models import Post, User
+from news.lib import fixtures
+
+class Command(BaseCommand):
+    admin_group_key = 'admin_group'
+    editor_group_key = 'editor_group'
+    user_group_key = 'user_group'
+
+    def handle(self, *args, **options):
+        groups = self.__create_groups()
+        self.__create_custom_permission(groups)
+        self.__create_users()
+    def __create_groups(self):
+        admin_group, created = Group.objects.get_or_create(name=ADMIN_GROUP_NAME)
+        editor_group, created = Group.objects.get_or_create(name=EDITOR_GROUP_NAME)
+        user_group, created = Group.objects.get_or_create(name=USER_GROUP_NAME)
+        return {
+            self.admin_group_key: admin_group,
+            self.editor_group_key: editor_group,
+            self.user_group_key: user_group
+        }
+
+    def __create_custom_permission(self, groups):
+        ct = ContentType.objects.get_for_model(Post)
+        permission = Permission.objects.create(codename='post_no_premoderation',
+                                               name='Can post without premoderation',
+                                               content_type=ct)
+        groups.get(self.user_group_key).permissions.add(permission)
+        groups.get(self.editor_group_key).permissions.add(permission)
+
+    def __create_users(self):
+        User.objects.create_admin(**fixtures.users.get('admin'))
+        User.objects.create_editor(**fixtures.users.get('editor'))
+        User.objects.create_user(**fixtures.users.get('user'))
+
+
--- news/models/user.py
+++ news/models/user.py
@@ -11,27 +11,27 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     birthdate = models.DateField(_('birth date'), null=True, blank=True)
-    
-    is_superuser=models.BooleanField(_('superuser'), default=False)
-    is_staff=models.BooleanField(_('staff'), default=False)
 
-    date_created=models.DateTimeField(_('date created'), auto_now_add=True)
-    date_updated=models.DateTimeField(_('date updated'), auto_now=True)
-    
-    objects=UserManager()
+    is_superuser = models.BooleanField(_('superuser'), default=False)
+    is_staff = models.BooleanField(_('staff'), default=False)
 
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=[]
+    date_created = models.DateTimeField(_('date created'), auto_now_add=True)
+    date_updated = models.DateTimeField(_('date updated'), auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name=_('user')
-        verbose_name_plural=_('users')
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def get_full_name(self):
         '''
         Returns the first_name plus the last_name, with a space in between.
         '''
-        full_name='%s %s' % (self.first_name, self.last_name)
+        full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
